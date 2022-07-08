f76e73d0f328cff915b30e6f12232dcc6fa29efc
jstuedle <jtstuedle@yahoo.com>
2022-07-08 11:52:48 -0500

    Update es.py
    Update verify_certs logic

--- shippers/es.py
+++ shippers/es.py
@@ -66,10 +66,10 @@ class ElasticsearchShipper(CommonShipper):
         else:
             raise ValueError("You must provide one between username and password or api_key")
             
-        if verify_certs:
-            es_client_kwargs["verify_certs"] = verify_certs
-        else:
+        if verify_certs is True or None:
             es_client_kwargs["verify_certs"] = True
+        else:
+            es_client_kwargs["verify_certs"] = False
 
         self._replay_args: dict[str, Any] = {}
 
